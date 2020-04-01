7fb54145c7730ab47279a7eb935379c8289c38cd
Duane Byer <duane.byer@gmail.com>
2020-03-31 20:14:10 -0400

    Add gravity to player.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,20 @@
 extends KinematicBody2D
 
-func _init() -> void:
-	var collision_shape := CollisionShape2D.new();
-	var shape := RectangleShape2D.new();
-	shape.set_extents(Vector2(16, 16));
-	collision_shape.set_shape(shape);
-	add_child(collision_shape);
+export var gravity := 300.0
+export var walk_speed := 300.0
+
+const _SNAP_DISTANCE := 2.0
+const _MAX_SLIDES := 5
+const _MAX_SLOPE_ANGLE := 0.25 * PI
+
+var _velocity := Vector2.ZERO
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta : float) -> void:
+	if !.is_on_floor():
+		self._velocity += self.gravity * delta * Vector2.DOWN
+	var snap := self._SNAP_DISTANCE * Vector2.DOWN
+	self._velocity = .move_and_slide_with_snap(self._velocity, snap, Vector2.UP, false, self._MAX_SLIDES, self._MAX_SLOPE_ANGLE, true);
+	
