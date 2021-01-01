052ba201fbbcf01a21725bf2f0179e3675b68694
Jamie <jamie@localhost.localdomain>
2021-01-01 14:56:39 -0800

    pick up and drop items

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+const Item := preload("res://scripts/Item.gd")
 
 export var gravity := 500.0
 export var walk_speed := 150.0
@@ -9,6 +10,11 @@ const _MAX_SLIDES := 5
 const _MAX_SLOPE_ANGLE := 0.25 * PI
 
 var _velocity := Vector2.ZERO
+
+onready var _area := $Area2D
+onready var _hand := $Hand
+var _held_item : Item = null
+var _held_item_parent : Node2D = null
 
 func _ready() -> void:
 	pass
@@ -30,3 +36,24 @@ func _physics_process(delta : float) -> void:
 	var snap := self._SNAP_DISTANCE * Vector2.DOWN
 	self._velocity = .move_and_slide_with_snap(self._velocity, snap, Vector2.UP, false, self._MAX_SLIDES, self._MAX_SLOPE_ANGLE, true);
 	
+
+func _process(delta : float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if _held_item == null:
+			var item_areas : Array = _area.get_overlapping_areas()
+			for item_area in item_areas:
+				var item : Node2D = item_area.get_parent()
+				if item is Item:
+					_held_item = item
+					_held_item_parent = item.get_parent()
+					item.get_parent().remove_child(_held_item)
+					_hand.add_child(_held_item)
+					_held_item.position = Vector2.ZERO
+					break
+		else:
+			var held_item_pos := _held_item.global_position
+			_hand.remove_child(_held_item)
+			_held_item_parent.add_child(_held_item)
+			_held_item.global_position = held_item_pos
+			_held_item = null
+			_held_item_parent = null
