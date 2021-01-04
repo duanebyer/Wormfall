9ee16f36a0a0e083e177dfc6bb4f93d753a28e88
Jamie <jamie@localhost.localdomain>
2021-01-04 13:52:36 -0800

    added online stuff and switching between rooms

---
+++ scripts/Door.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+export var leads_to : PackedScene = null
+
+func _ready():
+	pass # Replace with function body.
--- scripts/OnlinePlayer.gd
+++ scripts/OnlinePlayer.gd
@@ -18,37 +18,57 @@ onready var _hand := $Hand
 var _held_item : Item = null
 var _held_item_parent : Node2D = null
 
+
+puppet func setPosition(pos):
+	position = pos
+	
+master func shutItDown():
+	#Send a shutdown command to all connected clients, including this one
+	rpc("shutDown")
+	
+sync func shutDown():
+	get_tree().quit()
+
 func _ready() -> void:
 	pass
 
 func _physics_process(delta : float) -> void:
-	if !.is_on_floor():
-		self._velocity.y += self.gravity * delta
-	else:
-		if Input.is_action_just_pressed("jump"):
-			self._velocity.y = -self.jump_speed
+	if (is_network_master()):
+		if !.is_on_floor():
+			self._velocity.y += self.gravity * delta
+		else:
+			if Input.is_action_just_pressed("jump"):
+				self._velocity.y = -self.jump_speed
 
-	var move_dir := 0
-	if Input.is_action_pressed("walk_left"):
-		move_dir -= 1
-	if Input.is_action_pressed("walk_right"):
-		move_dir += 1
-	self._velocity.x = move_dir * self.walk_speed
+		var move_dir := 0
+		if Input.is_action_pressed("walk_left"):
+			move_dir -= 1
+		if Input.is_action_pressed("walk_right"):
+			move_dir += 1
+		self._velocity.x = move_dir * self.walk_speed
 
-	var snap := self._SNAP_DISTANCE * Vector2.DOWN
-	self._velocity = .move_and_slide_with_snap(self._velocity, snap, Vector2.UP, false, self._MAX_SLIDES, self._MAX_SLOPE_ANGLE, true);
+		var snap := self._SNAP_DISTANCE * Vector2.DOWN
+		self._velocity = self.move_and_slide_with_snap(self._velocity, snap, Vector2.UP, false, self._MAX_SLIDES, self._MAX_SLOPE_ANGLE, true);
+		
+		#Tell the other computer about our new position so it can update
+		#id as first argument if you only want to communicate to one computer
+		rpc_unreliable("setPosition", position)
 
 func _process(_delta : float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		if _held_item == null: #picking up an item
-			var collision_areas : Array = _area.get_overlapping_areas()
-			var indices = collision_indices(collision_areas)
-			if indices.x >= 0:
-				_pick_up_item(collision_areas[indices.x].get_parent())
-			elif indices.y >= 0:
-				_enter_door(collision_areas[indices.y].get_parent())
-		else: #dropping an item
-			_drop_item()
+	if (is_network_master()):
+		if Input.is_action_just_pressed("interact"):
+			if _held_item == null: #picking up an item
+				var collision_areas : Array = _area.get_overlapping_areas()
+				var indices = collision_indices(collision_areas)
+				if indices.x >= 0:
+					_pick_up_item(collision_areas[indices.x].get_parent())
+				elif indices.y >= 0:
+					_enter_door(collision_areas[indices.y].get_parent())
+			else: #dropping an item
+				_drop_item()
+		if Input.is_action_just_pressed("quit"):
+			if is_network_master():
+					shutItDown()
 		
 
 #think of a better way to do this prolly
